005b860f0efb14cf3e0837f391e0926876fd8d62
dieum512 <akonkwadieum222@gmail.com>
2023-06-20 22:12:08 +0200

    Fix the linters in music_album file

--- music_album.rb
+++ music_album.rb
@@ -2,14 +2,14 @@ require_relative 'genre'
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify, :publish_date
+  attr_accessor :on_spotify, :publish_date
 
-    def initializeid(on_spotify, publish_date)
-        super(publish_date)
-        @on_spotify = on_spotify
-    end
+  def initializeid(on_spotify, publish_date)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived
-        super && on_spotify == true
-    end
+  def can_be_archived
+    super && on_spotify == true
+  end
 end
