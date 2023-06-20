831183f867610b92a2ef7f73049f073af7dd7cb4
dieum512 <akonkwadieum222@gmail.com>
2023-06-20 14:09:35 +0200

    Implement the canbe archived and move to archive methods

--- item.rb
+++ item.rb
@@ -1,2 +1,20 @@
+require 'date'
+
 class Item
+
+    def initalize (publish_date)
+        @id = Random.rand(1...1000)
+        @publish_date = publish_date
+    end
+
+    def can_be_archived?
+        current_time = Time.now.year
+        published_time = Date.parse(publish_date).year
+        archive_cond = current_time - published_time
+        archive_cond > 10
+    end
+
+    def move_to_archive
+        @archived = can_be_archived?
+    end
 end
