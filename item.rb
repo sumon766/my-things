e02c0b4dd4e1bc207a0f969fc1bae6dede0087fc
dieum512 <akonkwadieum222@gmail.com>
2023-06-20 15:03:04 +0200

    Fix linter errors

--- item.rb
+++ item.rb
@@ -1,20 +1,19 @@
 require 'date'
 
 class Item
+  def initalize(publish_date)
+    @id = Random.rand(1...1000)
+    @publish_date = publish_date
+  end
 
-    def initalize (publish_date)
-        @id = Random.rand(1...1000)
-        @publish_date = publish_date
-    end
+  def can_be_archived?
+    current_time = Time.now.year
+    published_time = Date.parse(publish_date).year
+    archive_cond = current_time - published_time
+    archive_cond > 10
+  end
 
-    def can_be_archived?
-        current_time = Time.now.year
-        published_time = Date.parse(publish_date).year
-        archive_cond = current_time - published_time
-        archive_cond > 10
-    end
-
-    def move_to_archive
-        @archived = can_be_archived?
-    end
+  def move_to_archive
+    @archived = can_be_archived?
+  end
 end
