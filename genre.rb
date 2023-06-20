d99425a409651449fa8e4b4bbc493adfc454827f
dieum512 <akonkwadieum222@gmail.com>
2023-06-20 21:43:34 +0200

    Fix the linters in the genre.rb file

--- genre.rb
+++ genre.rb
@@ -1,14 +1,14 @@
 class Genre
-    attr_accessor :name, :items
+  attr_accessor :name, :items
 
-    def initialize(id, name)
-        @id = Random.rand(1...1000)
-        @name = name
-        @items = []
-    end
+  def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @item << item
-        item.genre = self
-    end
+  def add_item(item)
+    @item << item
+    item.genre = self
+  end
 end
