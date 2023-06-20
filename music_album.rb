05e95925795c7d858d1957c9f60829f31020bd3b
dieum512 <akonkwadieum222@gmail.com>
2023-06-20 22:10:38 +0200

    Add the methods to the MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,2 +1,15 @@
-class MusicAlbum
+require_relative 'genre'
+require_relative 'item'
+
+class MusicAlbum < Item
+    attr_accessor :on_spotify, :publish_date
+
+    def initializeid(on_spotify, publish_date)
+        super(publish_date)
+        @on_spotify = on_spotify
+    end
+
+    def can_be_archived
+        super && on_spotify == true
+    end
 end
