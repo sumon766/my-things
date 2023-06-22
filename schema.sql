b84af1e727b37688462587caffe0e265976fbfc2
dieum512 <akonkwadieum222@gmail.com>
2023-06-22 13:51:22 +0200

    Create the music_albums table

---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- Create music albums table
+CREATE TABLE music_albums (
+  id INT PRIMARY KEY,
+  publish_date DATE NOT NULL DEFAULT DATE,
+  on_spotify BOOLEAN,
+  archived BOOLEAN,
+  genre_id INT,
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
