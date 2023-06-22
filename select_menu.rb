552ca209d7b57df8113d477df664af6b4ae2f413
dieum512 <akonkwadieum222@gmail.com>
2023-06-22 22:30:42 +0200

    Update the select_menu file

--- select_menu.rb
+++ select_menu.rb
@@ -1,6 +1,10 @@
 def select_menu
   puts 'Please choose by entering the number'
-  puts '1. List all music albums'
-  puts '2. Add music album'
-  puts '3. Exit'
+  puts '1. List all books'
+  puts '2. List all labels'
+  puts '3. List all music albums'
+  puts '4. List all genres'
+  puts '5. Add a book'
+  puts '6. Add music album'
+  puts '7. Exit'
 end
