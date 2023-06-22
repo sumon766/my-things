f758c2eb9fd0ea6686ecdfd088e42894a0e30941
dieum512 <akonkwadieum222@gmail.com>
2023-06-22 13:52:06 +0200

    Create the genre table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE music_albums (
   genre_id INT,
   CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
+
+-- Create genres table
+CREATE TABLE genres (
+  id INT PRIMARY KEY,
+  name VARCHAR(100),
+);
