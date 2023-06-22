99d76a9ef9f2e75e92d2ed1b458d4a260bc422d0
dieum512 <akonkwadieum222@gmail.com>
2023-06-22 11:17:55 +0200

    Fix the linters in app.rb file

--- app.rb
+++ app.rb
@@ -3,21 +3,21 @@ require_relative 'music_album'
 require_relative 'genre'
 
 class App
-    def initialize
-        @music = ListMusic.new
-    end
+  def initialize
+    @music = ListMusic.new
+  end
 
-    def select_option(option)
-        case option
-        when '1'
-            @music.list_music_albums
-        end
+  def select_option(option)
+    case option
+    when '1'
+      @music.list_music_albums
     end
-    
-    def add_items(option)
-        case option
-        when '2'
-            @music.add_music_album
-        end
+  end
+
+  def add_items(option)
+    case option
+    when '2'
+      @music.add_music_album
     end
+  end
 end
