e36b0ddf4995a56300771d1a8b59a104e007c513
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-23 18:09:43 +0600

    Add test cases

---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../game'
+
+describe Game do
+  let(:title) { "Example Game" }
+  let(:publish_date) { Time.now }
+  let(:last_played_at) { Time.now - (3 * 365 * 24 * 60 * 60) } # Adjusted to represent 3 years in seconds
+  let(:multiplayer) { true }
+
+  describe "#initialize" do
+    it "creates a new game instance with the provided attributes" do
+      game = Game.new(title, publish_date, last_played_at, multiplayer)
+      expect(game.title).to eq(title)
+      expect(game.publish_date).to eq(publish_date)
+      expect(game.last_played_at).to eq(last_played_at)
+      expect(game.multiplayer).to eq(multiplayer)
+    end
+
+    it "assigns a unique ID to each game instance" do
+      game1 = Game.new(title, publish_date, last_played_at, multiplayer)
+      game2 = Game.new(title, publish_date, last_played_at, multiplayer)
+      expect(game1.id).not_to eq(game2.id)
+    end
+  end
+end  
---
+++ spec/item_spec.rb
@@ -0,0 +1,57 @@
+require_relative '../item'
+
+RSpec.describe Item do
+  let(:item) { Item.new('2020-01-01') }
+
+  describe '#initialize' do
+    it 'generates a random ID' do
+      expect(item.id).to be_a(Integer)
+    end
+
+    it 'sets the publish date' do
+      expect(item.publish_date).to eq('2020-01-01')
+    end
+
+    it 'sets archived to false by default' do
+      expect(item.archived).to be(false)
+    end
+
+    it 'initializes the label as nil' do
+      expect(item.label).to be_nil
+    end
+
+    it 'initializes the genre as nil' do
+      expect(item.genre).to be_nil
+    end
+
+    it 'initializes the author as nil' do
+      expect(item.author).to be_nil
+    end
+  end
+
+  describe '#can_be_archived?' do
+    it 'returns true when the item can be archived' do
+      item = Item.new((Time.now.year - 11).to_s + '-01-01')
+      expect(item.can_be_archived?).to be(true)
+    end
+
+    it 'returns false when the item cannot be archived' do
+      item = Item.new(Time.now.year.to_s + '-01-01')
+      expect(item.can_be_archived?).to be(false)
+    end
+  end
+
+  describe '#move_to_archive' do
+    it 'sets archived to true when the item can be archived' do
+      item = Item.new((Time.now.year - 11).to_s + '-01-01')
+      item.move_to_archive
+      expect(item.archived).to be(true)
+    end
+
+    it 'sets archived to false when the item cannot be archived' do
+      item = Item.new(Time.now.year.to_s + '-01-01')
+      item.move_to_archive
+      expect(item.archived).to be(false)
+    end
+  end
+end
